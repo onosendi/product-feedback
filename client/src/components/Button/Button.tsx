b8db15c02f18d69d6bcb93dfa06c3eccea8ad2c5
Daniel Lindegren <onosendi@dlindegren.com>
2021-10-07 19:40:14 -0700

    TS fro Button component stopping point

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,11 +1,24 @@
 import cx from 'clsx';
 import PropTypes from 'prop-types';
-import { forwardRef } from 'react';
+import { forwardRef, ReactNode } from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './Button.module.scss';
 
-const Button = forwardRef(({
+interface ButtonProps {
+  children: ReactNode,
+  className: string | null,
+  fullWidth: boolean,
+  href: string | null,
+  prependPlus: boolean,
+  selected: boolean,
+  type: 'button' | 'submit',
+  variant: '1' | '2' | '3' | '4' | '5',
+}
+
+type ButtonRef = HTMLButtonElement | HTMLAnchorElement;
+
+const Button = forwardRef<ButtonRef, ButtonProps>(({
   children,
   className,
   fullWidth,
@@ -32,6 +45,7 @@ const Button = forwardRef(({
 
   return href
     ? <Link to={href} href={href} {...commonProps}>{children}</Link>
+    // eslint-disable-next-line react/button-has-type
     : <button type={type} {...commonProps}>{children}</button>;
 });
 
