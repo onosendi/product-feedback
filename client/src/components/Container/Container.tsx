4269f9010d33a3060be1321d45a4bea5924bdc77
Daniel Lindegren <onosendi@dlindegren.com>
2021-10-16 17:47:12 -0700

    Finish typing on Container component

--- client/src/components/Container/Container.tsx
+++ client/src/components/Container/Container.tsx
@@ -1,23 +1,26 @@
 import cx from 'clsx';
-import PropTypes from 'prop-types';
-import { forwardRef } from 'react';
+import { ElementType, forwardRef, ReactNode } from 'react';
 import styles from './Container.module.scss';
 
-const Container = forwardRef(({
+type ContainerProps = {
+  children: ReactNode,
+  className?: string | null,
+  component?: ElementType | string,
+  noWrapper?: boolean,
+  wrapperClassName?: string | null,
+  wrapperComponent?: ElementType | string,
+};
+
+const Container = forwardRef<HTMLElement, ContainerProps>(({
   children,
-  className,
-  component: Component,
-  noWrapper,
-  wrapperClassName,
-  wrapperComponent: WrapperComponent,
-  ...props
-}, fRef) => {
+  className = null,
+  component: Component = 'div',
+  noWrapper = false,
+  wrapperClassName = null,
+  wrapperComponent: WrapperComponent = 'div',
+}, forwardedRef) => {
   const component = (
-    <Component
-      className={cx(styles.container, className)}
-      ref={fRef}
-      {...props}
-    >
+    <Component className={cx(styles.container, className)} ref={forwardedRef}>
       {children}
     </Component>
   );
@@ -32,25 +35,5 @@ const Container = forwardRef(({
 
   return component;
 });
-
-Container.displayName = 'Container';
-
-Container.defaultProps = {
-  children: null,
-  className: null,
-  component: 'div',
-  noWrapper: false,
-  wrapperClassName: null,
-  wrapperComponent: 'div',
-};
-
-Container.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-  component: PropTypes.oneOfType([PropTypes.string, PropTypes.shape({})]),
-  noWrapper: PropTypes.bool,
-  wrapperClassName: PropTypes.string,
-  wrapperComponent: PropTypes.string,
-};
 
 export default Container;
