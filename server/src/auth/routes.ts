b479e5f9ccec50a11cd36bed1b2e6ff4485ddcf4
Daniel Lindegren <onosendi@dlindegren.com>
2021-10-17 21:46:17 -0700

    Combine controllers/routes into more hapi-like config

--- server/src/auth/routes.ts
+++ server/src/auth/routes.ts
@@ -1,22 +1,28 @@
-import { FastifyPluginCallback, RouteShorthandOptionsWithHandler } from 'fastify';
-import { token } from './controllers';
+import { FastifyPluginCallback } from 'fastify';
 
-const tokenOptions = {
-  handler: token,
-  schema: {
-    body: {
-      type: 'object',
-      required: ['password', 'username'],
-      properties: {
-        password: { type: 'string' },
-        username: { type: 'string' },
+const authRoutes: FastifyPluginCallback = (fastify, opts, done) => {
+  fastify.route<{
+    Body: { username: string, password: string },
+  }>({
+    url: '/token',
+    method: 'POST',
+    handler: async (request) => {
+      const { username, password = '' } = request.body;
+      // eslint-disable-next-line no-console
+      console.log(username, password);
+      return { foo: 'bar' };
+    },
+    schema: {
+      body: {
+        type: 'object',
+        required: ['password', 'username'],
+        properties: {
+          password: { type: 'string' },
+          username: { type: 'string' },
+        },
       },
     },
-  },
-};
-
-const authRoutes: FastifyPluginCallback = (fastify, options, done) => {
-  fastify.post('/token', tokenOptions as RouteShorthandOptionsWithHandler);
+  });
   done();
 };
 
