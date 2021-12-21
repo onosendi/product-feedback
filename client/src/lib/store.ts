0d125884c7ddacb54a042c5fdd4c1b1a1e563386
Daniel Lindegren <onosendi@dlindegren.com>
2021-12-20 20:55:17 -0700

    Remove rootReducer - we're not using 'auth/logout' action type anymore to reset reducer to initial state

--- client/src/lib/store.ts
+++ client/src/lib/store.ts
@@ -1,11 +1,4 @@
-// TODO: Remove `rootReducer` since auth/logout shouldn't wipe the store
-// to its initial state.
-import {
-  AnyAction,
-  combineReducers,
-  configureStore,
-  getDefaultMiddleware,
-} from '@reduxjs/toolkit';
+import { combineReducers, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import persistStore from 'redux-persist/es/persistStore';
 import storage from 'redux-persist/lib/storage';
@@ -22,23 +15,13 @@ const combinedReducers = combineReducers({
   [suggestionsApi.reducerPath]: suggestionsApi.reducer,
 });
 
-function rootReducer(
-  state: ReturnType<typeof combinedReducers> | undefined,
-  action: AnyAction,
-) {
-  if (action.type === 'auth/logout') {
-    return combinedReducers(undefined, { type: undefined });
-  }
-  return combinedReducers(state, action);
-}
-
 const persistConfig = {
   key: 'root',
   storage,
   whitelist: ['auth'],
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, combinedReducers);
 
 export const store = configureStore({
   middleware: getDefaultMiddleware({
