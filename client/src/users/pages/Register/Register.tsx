bb43059e81cb37dfc1cf3ee140957b2e2780922d
Daniel Lindegren <onosendi@dlindegren.com>
2021-12-31 17:02:46 -0700

    Remove random property from Register component

--- client/src/users/pages/Register/Register.tsx
+++ client/src/users/pages/Register/Register.tsx
@@ -84,7 +84,6 @@ export default function Register() {
                     validate={composeValidators(
                       [isFilled],
                     )}
-                    someOtherField="foo"
                     render={({ input, meta }) => (
                       <TextField
                         hasError={getHasError(meta)}
