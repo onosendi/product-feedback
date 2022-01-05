66eee20941ea10d50978f4372a566c3d1a0b3805
Daniel Lindegren <onosendi@dlindegren.com>
2022-01-05 12:00:46 -0700

    Roadmap stopping point

--- client/src/suggestions/pages/Roadmap/Roadmap.tsx
+++ client/src/suggestions/pages/Roadmap/Roadmap.tsx
@@ -1,11 +1,23 @@
+import type { DBSuggestionStatus } from '@t/database';
 import cx from 'clsx';
+import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Button, Container, GoBack } from '../../../components';
 import { APP_NAME } from '../../../lib/constants';
 import routes from '../../../lib/routes';
 import styles from './Roadmap.module.scss';
 
+type ShowStatusState = Omit<DBSuggestionStatus, 'suggestion'>;
+
 export default function Roadmap() {
+  const [showStatus, setShowStatus] = useState<ShowStatusState>('planned');
+
+  const mobileNavButtons = [
+    { status: 'planned', text: 'Planned', show: showStatus === 'planned' },
+    { status: 'in-progress', text: 'In Progress', show: showStatus === 'in-progress' },
+    { status: 'live', text: 'Live', show: showStatus === 'live' },
+  ];
+
   return (
     <>
       <Helmet>
@@ -36,7 +48,20 @@ export default function Roadmap() {
         component="nav"
         wrapperClassName={cx(styles.statusButtonWrapper)}
       >
-        Foo
+        {mobileNavButtons.map((button) => (
+          <button
+            className={cx(
+              'type-4',
+              styles.statusButton,
+              button.show && styles.statusButtonActive,
+            )}
+            key={button.status}
+            onClick={() => { setShowStatus(button.status); }}
+            type="button"
+          >
+            {button.text}
+          </button>
+        ))}
       </Container>
       <Container
         className={cx(styles.statusColumns)}
