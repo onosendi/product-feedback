4502987a092075bfdd1503e4bc588976a8f044e4
Daniel Lindegren <onosendi@dlindegren.com>
2022-01-13 17:03:50 -0700

    Style Error404 page

--- client/src/project/components/Error404/Error404.tsx
+++ client/src/project/components/Error404/Error404.tsx
@@ -1,6 +1,18 @@
-// import styles from './Error404.module.scss';
+import cx from 'clsx';
+import { Helmet } from 'react-helmet-async';
+import { APP_NAME } from '../../constants';
+import styles from './Error404.module.scss';
 
 export default function Error404() {
-  // TODO
-  return <p>404 - Page not found</p>;
+  return (
+    <>
+      <Helmet>
+        <title>{`404: This page could not be found - ${APP_NAME}`}</title>
+      </Helmet>
+      <div className={cx(styles.container)}>
+        <h1 className={cx(styles.heading)}>404</h1>
+        <p className={cx(styles.body)}>This page could not be found</p>
+      </div>
+    </>
+  );
 }
